b0fbcf8954b8256f5d9b5bd82e30fb185d198759
ayunimustafa <ayunimustafa98@gmail.com>
2023-02-02 21:55:46 +0800

    commit ui_widget

--- lib/pages/logout_page.dart
+++ lib/pages/logout_page.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mahallahfriendfinder/auth.dart';
+import 'package:mahallahfriendfinder/routes.dart';
+import 'package:mahallahfriendfinder/widgets/ui_widget.dart';
 
 class LogoutPage extends StatefulWidget {
   const LogoutPage({Key? key}) : super(key: key);
@@ -10,9 +13,48 @@ class LogoutPage extends StatefulWidget {
 }
 
 class _LogoutPageState extends State<LogoutPage> {
+  static const int counter = 5;
+  double value = counter.toDouble();
+  late Timer timer;
+
+  void _ timer() {
+    timer = Timer.periodic (const Duration(seconds:1), (timer) {
+      setState(() {
+        if (value <= 0){
+          timer.cancel();
+        }
+        value--;
+      });
+    })
+  }
+
+  void _ timerPop (){
+    timer = Timer.periodic(Duration(seconds: counter + 1), (timer) {
+      Navigator.popUntil (context, ModalRoute.withName(Routes.loginPage));
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    _timer();
+    _timerPop();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text ('Logout'),
+        centerTitle: true,
+      )
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.fromLTRB(30, 40, 30, 30),
+        child: Column (children: <Widget>[
+          Text ("You have successfully logged out. Redirecting in $value seconds..."),
+          sizedBox(0,50),
+          linearLoading(value/counter),
+        ]),
+      )
+    );
+
+
+    )
   }
 }
 
